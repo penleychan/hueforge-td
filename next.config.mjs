81fe961e4891854a828e2dfd8ba4b6c9bb10ccc7
Penley Chan <penleychan@gmail.com>
2024-06-16 00:47:04 -0600

    update config for vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: 'hueforge-td',
-};
+const nextConfig = {};
 
 export default nextConfig;
